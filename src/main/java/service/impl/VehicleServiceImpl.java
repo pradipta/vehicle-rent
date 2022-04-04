1300a4c8e164a8048d8e968be2d50c2029a3709d
pradipta-sarma <pradipta.sarma@BLRETV-C02F4A73.local>
2022-04-04 23:46:53 +0530

    Removed unsuppoeted Java 11 .toList()

--- src/main/java/service/impl/VehicleServiceImpl.java
+++ src/main/java/service/impl/VehicleServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static model.Vehicle.currentCentreId;
 
@@ -62,7 +63,7 @@ public class VehicleServiceImpl implements VehicleService {
         return branch.getVehicles().stream()
                 .filter(vehicle -> isAvailable(vehicle, startTime, endTime))
                 .sorted((vehicle1, vehicle2) -> vehicle1.getPricePerSlot().compareTo(vehicle2.getPricePerSlot()))
-                .toList();
+                .collect(Collectors.toList());
     }
 
     @Override
